a55552b742bf65009673f88282079a36a86755f1
patriciochavez <patricio_chavez@hotmail.com>
2019-11-13 18:21:44 -0300

    Update install_minikube_jx.sh

--- install_minikube_jx.sh
+++ install_minikube_jx.sh
@@ -1,22 +1,21 @@
 SECONDS=0
 sudo apt update
 sudo apt install -y apt-transport-https ca-certificates curl software-properties -common
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable"
 sudo apt update
 sudo apt upgrade -y
 sudo apt install -y docker-ce
 sudo systemctl enable docker
 sudo snap install kubectl --classic
-wget http://archive.ubuntu.com/ubuntu/pool/main/m/mesa/libgl1-mesa-dri_19.0.8-0u
-buntu0~18.04.3_amd64.deb
+sudo wget http://archive.ubuntu.com/ubuntu/pool/main/m/mesa/libgl1-mesa-dri_19.0.8-0ubuntu0~18.04.3_amd64.deb
 sudo dpkg -i libgl1-mesa-dri_19.0.8-0ubuntu0~18.04.3_amd64.deb
 sudo apt-get -fy install
 sudo apt install -y virtualbox
-curl -Lo minikube https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64
-chmod +x minikube
+sudo curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube_1.5.2.deb && sudo dpkg -i minikube_1.5.2.deb
+sudo rm -f minikube_1.5.2.deb
 sudo cp minikube /usr/local/bin
-rm minikube
+sudo rm -f minikube
 sudo minikube start --vm-driver=none
 sudo apt install -y socat
 sudo snap install helm --classic
