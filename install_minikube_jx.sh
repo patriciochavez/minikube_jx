91475a7a7bd8933efee8ca8d6951cb1d937f85d3
patriciochavez <patricio_chavez@hotmail.com>
2019-11-13 20:39:46 -0300

    Update install_minikube_jx.sh

--- install_minikube_jx.sh
+++ install_minikube_jx.sh
@@ -1,6 +1,6 @@
 SECONDS=0
 sudo apt update
-sudo apt install -y apt-transport-https ca-certificates curl software-properties -common
+sudo apt install -y apt-transport-https ca-certificates curl software-properties-common
 sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable"
 sudo apt update
